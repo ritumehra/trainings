05e64b3434c77625f30000144badb26e5cddc170
Ritu Chawla <rchawla.mail@gmail.com>
2018-08-12 10:26:58 +0530

    identifiers_python.py

--- keywords_python.py
+++ keywords_python.py
@@ -1,5 +1,14 @@
 # Example keywords
 
 import keyword
-
 print(keyword.kwlist)
+
+# Cannot use keywords as variable/function names
+# if = 1
+
+# Case Sensitive
+
+train_var = " Training"
+train_Var = "Training Var"
+train_var = " Training Var"
+print(train_var,"\n",train_Var)
