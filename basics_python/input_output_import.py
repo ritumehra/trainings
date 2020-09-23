4f28e734685b81a836921af6925b61dfd24e239e
RituChawla <ritu.chawla@yaana.com>
2020-09-23 18:13:12 +0530

    New Files Added

---
+++ basics_python/input_output_import.py
@@ -0,0 +1,14 @@
+import json
+
+var_json = json.dumps({1: 'a', 2: "b"})
+
+print(var_json)
+
+num = input('Enter a number: ')
+
+if num == 10:
+    print("Match")
+else:
+    print("No-Match")
+
+print(num)
